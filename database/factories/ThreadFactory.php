0636fa357034bfa4725d2a5d68077c00aa4d2704
Christian <chrisdaq@gmail.com>
2022-12-28 10:27:44 -0400

    created models, controllers, factories using artisan

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Thread>
+ */
+class ThreadFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),// a thread belongs to user
+            'title' => fake()->sentence,
+            'body' => fake()->paragraph
+        ];
+    }
+}
