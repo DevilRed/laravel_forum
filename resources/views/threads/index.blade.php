c55a01ecc306f9b0d0a90d1c20c696f873f44076
Christian <chrisdaq@gmail.com>
2022-12-29 11:53:05 -0400

    make changes to get first test passing

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Forum Threads') }}</div>
+
+                    <div class="card-body">
+                        @foreach($threads as $thread)
+                            <article>
+                                <h4>{{ $thread->title }}</h4>
+                                <div class="body">{{ $thread->body }}</div>
+                            </article>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ThreadsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/threads', [ThreadsController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
