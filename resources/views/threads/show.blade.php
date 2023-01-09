fdd9e185fd9f02f15d6cf379b197294064bc24ba
Christian <chrisdaq@gmail.com>
2023-01-09 17:58:27 -0400

    add form to add replies in show template

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -21,9 +21,26 @@
             <div class="col-md-12">
 
                     @foreach($thread->replies as $reply)
-                        @include('threads.reply');
+                        @include('threads.reply')
                     @endforeach
             </div>
         </div>
+
+    @if(auth()->check())
+        <div class="row">
+            <div class="col-md-12">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+
+                    <button class="btn btn-default" type="submit">Post</button>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+    @endif
     </div>
 @endsection
