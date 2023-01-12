3577d72daef21f56cc7258635216f8d33b9d5c11
Christian <chrisdaq@gmail.com>
2023-01-12 12:42:30 -0400

    add testing utilities to avoid duplication

---
+++ tests/utilities/functions.php
@@ -0,0 +1,8 @@
+<?php
+function create($class, $attributes = []) {
+    return $class::factory()->create($attributes);
+}
+
+function make($class, $attributes = []) {
+    return $class::factory()->make($attributes);
+}
