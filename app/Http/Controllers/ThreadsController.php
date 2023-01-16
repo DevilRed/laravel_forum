8d52120a515cb3827fcc56e481f4777d956089ea
Christian <chrisdaq@gmail.com>
2023-01-16 11:35:16 -0400

    order threads by id desc

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -21,7 +21,7 @@ class ThreadsController extends Controller
      */
     public function index()
     {
-        $threads = Thread::all();
+        $threads = Thread::orderBy('id', 'desc')->get();
         return view('threads.index', compact('threads'));
     }
 
