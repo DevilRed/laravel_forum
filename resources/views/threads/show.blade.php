41225d1aa2c83b4cf5d4c2be55b4225d9473ebdd
Christian <chrisdaq@gmail.com>
2023-01-27 12:41:56 -0400

    use laravel helper to pluralize word if replies_count is more than 1

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -39,7 +39,8 @@
                 <div class="card">
                     <div class="card-body">
                         <p>
-                            This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="#">{{ $thread->creator->name }}</a> and currently has {{ $thread->replies_count }} comments
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by <a href="#">{{ $thread->creator->name }}</a> and currently has {{ $thread->replies_count }}
+                            {{Str::plural('comment', $thread->replies_count)}}
                         </p>
                     </div>
                 </div>
