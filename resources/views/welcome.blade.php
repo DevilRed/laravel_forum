6da35cb451b1a1ea0415d78dfd46cc0f405fa8fe
Christian <chrisdaq@gmail.com>
2023-01-28 16:12:27 -0400

    fix referencing issues in blade template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,14 @@
     <title>Laravel Multistep Livewire Form Example</title>
     @livewireStyles
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" rel="stylesheet" id="bootstrap">
-    <link href="{{ asset('multiform.css') }}" rel="stylesheet" id="bootstrap">
+    <link href="{{ asset('form/multiform.css') }}" rel="stylesheet" id="bootstrap">
 </head>
 <body class="mt-5">
 <div class="container">
     <div class="text-center">
         Laravel Form Wizard Example
     </div>
-    <livewire:wizard />
+    <livewire:form.wizard />
 </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
