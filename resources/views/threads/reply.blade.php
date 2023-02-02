d0a774d283f3c2db8aa35f79abba9376c244adea
Christian <chrisdaq@gmail.com>
2023-02-02 18:15:35 -0400

    add favorite button in threads reply template

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,7 +1,19 @@
 <div class="card">
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a> said
-        {{ $reply->created_at->diffForHumans() }}...
+        <div class="level">
+            <h5 class="flex">
+                <a href="#">{{ $reply->owner->name }}</a> said
+                {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+        </div>
+        <div>
+            <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                {{ csrf_field() }}
+                <button type="submit" class="btn btn-outline-primary" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                    {{ $reply->favorites()->count() }} {{ Str::plural('Favorite', $reply->favorites()->count()) }}
+                </button>
+            </form>
+        </div>
     </div>
     <div class="card-body">
         {{ $reply->body }}
