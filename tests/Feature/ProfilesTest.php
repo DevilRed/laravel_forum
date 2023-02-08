b8b8e0db6dfc83562e08f2267e1e507fcaaee5cc
Christian <chrisdaq@gmail.com>
2023-02-08 17:11:21 -0400

    add test to load threads made from a user

--- tests/Feature/ProfilesTest.php
+++ tests/Feature/ProfilesTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\Feature;
 
+use App\Models\Thread;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -16,5 +17,17 @@ class ProfilesTest extends TestCase
 
         $this->get('/profiles/' . $user->name)
             ->assertSee($user->name);
+    }
+
+
+    /** @test */
+    public function profiles_display_all_th_reads_created_by_the_associated_user()
+    {
+        $user = create(User::class);
+        $thread = create(Thread::class, ['user_id' => $user->id]);
+
+        $this->get('/profiles/' . $user->name)
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
     }
 }
