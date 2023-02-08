0c9bcb0f6cfa112868af00c552c96d595f4fdb67
Christian <chrisdaq@gmail.com>
2023-02-08 17:12:25 -0400

    format view

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,5 +8,25 @@
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
+
+        @foreach($profileUser->threads as $thread)
+            <div class="card">
+                <div class="card-header">
+                    <div class="level">
+                        <span class="flex">
+                            <a href="#">{{$thread->creator->name }}</a> posted:
+                    {{$thread->title }}
+                        </span>
+
+                        <span>{{ $thread->created_at->diffForHumans() }}</span>
+                    </div>
+
+                </div>
+
+                <div class="card-body">
+                    {{ $thread->body }}
+                </div>
+            </div>
+        @endforeach
     </div>
 @endsection
