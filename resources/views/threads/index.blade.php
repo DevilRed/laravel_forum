48cbf2ff9c594de11a9e2cced61768a865d48666
Christian <chrisdaq@gmail.com>
2023-02-09 15:57:30 -0400

    change layout at threads index

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,25 +4,22 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">{{ __('Forum Threads') }}</div>
+                @foreach($threads as $thread)
+                    <div class="card mt-3">
+                        <div class="card-header">
+                            <div class="level">
+                                <h4 class="flex">
+                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                </h4>
 
-                    <div class="card-body">
-                        @foreach($threads as $thread)
-                            <article>
-                                <div class="level">
-                                    <h4 class="flex">
-                                        <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                    </h4>
-
-                                    <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
-                                </div>
-                                <div class="body">{{ $thread->body }}</div>
-                            </article>
-                        @endforeach
-
+                                <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ Str::plural('reply', $thread->replies_count) }}</a>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="body">{{ $thread->body }}</div>
+                        </div>
                     </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
