cfd5b8c9a87ea3e81e79dcc3efb57019c6720c88
Christian <chrisdaq@gmail.com>
2023-02-14 17:16:36 -0400

    remove unnecessary content and refactor profiles controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,12 +9,21 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        $activities = $user->activity()->latest()->with('subject')->get()->groupBy(function($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
+        // get user activities with eager loading, grouping by date and paginating by 50
         return view('profiles.show',[
             'profileUser' => $user,
-            'activities' => $activities
+            'activities' => $this->getActivity($user)
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    protected function getActivity(User $user): \Illuminate\Database\Eloquent\Collection
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
